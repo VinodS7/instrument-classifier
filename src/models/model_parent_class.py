3765a5db1b49e9fc6144f4ef9784fa2a71624680
Vinod Subramanian <vs308@bath.eecs.qmul.ac.uk>
2019-02-12 19:27:05 +0000

    Created parent python class for the models

---
+++ src/models/model_parent_class.py
@@ -0,0 +1,10 @@
+from __future__ import absolute_import,print_function,division
+
+import tensorflow as tf
+import numpy as np
+
+
+class ModelsParentClass():
+    def __init__(self):
+        print("created")
+    
