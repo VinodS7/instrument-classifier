2b99efaebc03599e4c94417878dfe72d110da1c5
aframires <aframires@gmail.com>
2019-03-01 15:44:09 +0100

    Updated parent class

--- src/models/model_parent_class.py
+++ src/models/model_parent_class.py
@@ -1,10 +1,31 @@
-from __future__ import absolute_import,print_function,division
+from __future__ import absolute_import, print_function, division
 
 import tensorflow as tf
 import numpy as np
 
 
 class ModelsParentClass():
-    def __init__(self):
-        print("created")
-    
+
+    def read_from_tfrecord(self, data_record):
+        tfrecord_features = tf.parse_single_example(data_record, features={'spec': tf.FixedLenFeature([], tf.string),
+                                                                           'shape': tf.FixedLenFeature([], tf.string),
+                                                                           'label': tf.FixedLenFeature([], tf.int64)},
+                                                    name='features')
+        shape = tf.decode_raw(tfrecord_features['shape'], tf.int32)
+        spec = tf.decode_raw(tfrecord_features['spec'], tf.float32)
+        spec = tf.reshape(spec, shape)
+        label = tfrecord_features['label']
+        return spec, label
+
+    def compute_accuracy(self, logits, labels, nb_classes):
+        probs = tf.nn.softmax(logits)
+        prediction = tf.argmax(probs, 1)
+        equality = tf.equal(tf.cast(prediction, tf.int64), labels)
+        accuracy = tf.reduce_mean(tf.cast(equality, tf.float32))
+        cm = tf.confusion_matrix(labels, prediction, num_classes=nb_classes)
+        return accuracy, cm
+
+    def print_message(self):
+        print("Works so far")
+
+
